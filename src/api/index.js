76bb6575fb08553f48faee992df82df01e39e58c
wudiha <mysterypma@gmail.com>
2022-01-18 22:41:17 +0800

    gotta remove after authentication done.

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,7 @@ const api = {
 		return 'https://accounts.spotify.com/authorize?client_id=3f6dec98d9e44b9ea256675711d16ba8&response_type=code&redirect_uri=http://localhost:8080&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
 	},
 	getMusicList() {
+		//api testing , not actual music list api
 		return axios.get('https://covidtracking.com/api/states');
 	},
 };
